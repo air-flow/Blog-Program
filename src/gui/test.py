5bd799e40d74c29fc8e991eb4da9e87948fb08e1
updraft <updraft.1970@gmail.com>
2021-05-04 00:57:52 +0900

    feat:text file path get

--- src/gui/test.py
+++ src/gui/test.py
@@ -1,16 +1,28 @@
+import os
 import PySimpleGUI as sg
-
 sg.theme('DarkAmber')
+
+
+def cd():
+    os.chdir(os.path.dirname(__file__))
 
 
 def ElementSetUp():
     layout = [[sg.Text('Some text on Row 1')],
               [sg.Text('Enter something on Row 2'), sg.InputText()],
-              [sg.Combo(['test', 'test2'], default_value="test")],
+              [sg.Text('実行ファイル選択'), sg.Combo(
+                  ['test', 'test2'], default_value="test")],
               [sg.Button('Ok'), sg.Button('Cancel')]
               #   []
               ]
     return layout
+
+
+def GetFileList():
+    cd()
+    with open("../../mine/pathfile.txt", encoding="utf-8") as target:
+        text = target.readline()
+    print(text)
 
 
 def main():
@@ -21,11 +33,13 @@ def main():
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
             break
-        print('You entered ', values)
+        print('You entered ', values, event)
 
     window.close()
 
 
 if __name__ == "__main__":
-    main()
-    ElementSetUp()
+    # main()
+    # ElementSetUp()
+    pass
+    GetFileList()
